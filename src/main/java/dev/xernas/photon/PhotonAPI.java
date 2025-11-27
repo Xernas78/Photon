970b0a3d0155c6ad38daf8443d15c84905574c63
Xernas Dev <71571183+Xernas78@users.noreply.github.com>
2025-11-27 19:52:14 +0100

    Changed version

--- src/main/java/dev/xernas/photon/PhotonAPI.java
+++ src/main/java/dev/xernas/photon/PhotonAPI.java
@@ -12,7 +12,7 @@ import dev.xernas.photon.api.window.Window;
 public class PhotonAPI {
 
     public static final String NAME = "Photon";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "2.0.0";
 
     private static Library library;
     private static boolean initialized = false;
